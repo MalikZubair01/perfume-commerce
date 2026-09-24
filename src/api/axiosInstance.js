308cd384f8bbb2e4208332e49bd101cb87620794
MalikZubair01 <malikzubairmaqsood@gmail.com>
2026-09-24 19:35:00 +0500

    update base url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -31,7 +31,7 @@ export const getStoredAdmin = () => {
 };
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL ,
   headers: {
     "Content-Type": "application/json",
   },
